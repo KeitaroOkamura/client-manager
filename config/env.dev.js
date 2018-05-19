d1ebb854c843afa88d7d5214139a048576b1d407
dtm-okamura <okamura@dt-media.jp>
2018-05-20 02:44:34 +0900

    envファイル作成

---
+++ config/env.dev.js
@@ -0,0 +1,11 @@
+const ENV = {
+  HOST: 'localhost',
+  USER: 'root',
+  PASSWORD: 'keitarou',
+  DATABASE: 'client_manager',
+  DEBUG: false
+}
+
+module.exports = {
+  ENV
+}
---
+++ config/env.heroku.js
@@ -0,0 +1,11 @@
+const ENV = {
+  HOST: 'us-cdbr-iron-east-04.cleardb.net',
+  USER: 'bcb1b5d923023e',
+  PASSWORD: '4f58e167',
+  DATABASE: 'heroku_0a4dd7a99c673ff',
+  DEBUG: false
+}
+
+module.exports = {
+  ENV
+}
